0ebc13ae08a5f610c7df3a7d8bd7fa8ac2360af7
charitraa <stharabi9862187405@gmail.com>
2024-02-19 16:14:01 +0545

    creating new design

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var typed = new Typed('.change', {
+  strings: ['Software Developer', 'Full Stack Developer', 'Ethical Hacker'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
+document.addEventListener('DOMContentLoaded', function () {
+  var mixer = mixitup('.portfolio-gallery')
+})
 const circle = document.querySelectorAll('.circle')
 circle.forEach(elem => {
   var dots = elem.getAttribute('data-dots')
